92d29048891c2705bd2f0d378386e5a48360d29e
IanCon <ianconradie@yahoo.com>
2017-04-02 12:46:34 +0100

    Updated steps

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Create a tidy data set\
 
 ## Step 1
@@ -33,3 +34,38 @@ A new data set is created from the list of activity labels and the source data o
 ## Step 8
 As per the assignment, an second tidy data set is then created from the first tidy data set, by using the activity to 
 aggregate the average (mean) of the standard deviation and the mean values and grouping the averages by the subject and activity.
+=======
+# Create a tidy data set
+
+## Step 1 
+The first section copies the source file from the URL to the local working directory and unzips it
+ 
+Having used the file explorer to see what files are in the unzipped directory, the various files are 
+used to create some R data sets for processing. 
+ 
+## Step 2 
+The second section creates data frames of the activity labels and feature names for later use
+ 
+## Step 3 
+In the third section the row and column (X and Y coordinates) plus subject details of both 
+the training dataset from the source are copied into R structures, and the feature 
+names, activity and subject ids are used to rename the default R naming. 
+ 
+## Step 4 
+In the fourth section the test datasets are copied columns renamed 
+... and then the test and training datsets are combined /merged
+
+## Step 5 
+The column names are then used to extract out of the complete dataset only the mean,  
+standard deviation and id columns. 
+ 
+## Step 6 
+The column names are renamed to follow tidy data set requirements - i.e. camel case, and any 
+non-alphabetic characters []()- are removed. 
+ 
+## Step 7 
+Add the activity name to aid readability   
+ 
+## Step 8 
+As per the assignment, create a second tidy data set by averaging the aggregated mean and standard deviation values and grouping these by subject and activity. 
+>>>>>>> Stashed changes
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,17 @@
 library(reshape2)
 
-# Read the source files
+# Step 1 - Read the source files
 if(!file.exists('./data')){dir.create('./data')}
 fileUrl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 download.file(fileUrl,destfile='./data/adlData.zip')
 unzip('./data/adlData.zip')
 
-# Get the label and feature details
+# Step 2 - Get the label and feature details
 activity_labels <- read.table('./UCI HAR Dataset/activity_labels.txt')  # 6 categories: walking, sitting etc in V2, V1 = 1 to 6
 features <- read.table('./UCI HAR Dataset/features.txt')
 colnames(activity_labels) <- c('activityId', 'activityType')    # adding activity type to each row improves readability
 
-# Load and name the training dataset details
+# Step 3 Load and name the training dataset details
 X_train <- read.table('./UCI HAR Dataset/train/X_train.txt')
 y_train <- read.table('./UCI HAR Dataset/train/y_train.txt')
 subject_train <- read.table('./UCI HAR Dataset/train/subject_train.txt')
@@ -19,7 +19,7 @@ colnames(X_train) <- features[, 2]
 colnames(y_train) <- 'activityId'
 colnames(subject_train) <- 'subjectId'
 
-# Load and name the test dataset details
+# Step 4 - Load and name the test dataset details
 X_test <- read.table('./UCI HAR Dataset/test/X_test.txt')
 y_test <- read.table('./UCI HAR Dataset/test/y_test.txt')
 subject_test <- read.table('./UCI HAR Dataset/test/subject_test.txt')
@@ -27,7 +27,7 @@ colnames(X_test) <- features[, 2]
 colnames(y_test) <- 'activityId'
 colnames(subject_test) <- 'subjectId'
 
-# Merge the test and training datasets
+# ... and merge the test and training datasets
 train_dataset <- cbind(subject_train, y_train, X_train)
 test_dataset <- cbind(subject_test, y_test, X_test)
 merged_dataset <- rbind(train_dataset, test_dataset)
@@ -35,18 +35,18 @@ merged_dataset <- rbind(train_dataset, test_dataset)
 # Column names from the dataset
 col_names <- colnames(merged_dataset)
 
-# Use the column names to only select the columns that end in mean, std, or Id 
+# Step 5 - Use the column names to only select the mean, standard deviation and id columns 
 mean_std_dataset <- merged_dataset[grep('.*mean.*|.*std.*|.*Id', col_names)]
 
-# Rename the column names to capitalise Mean and Std, and remove non-alphabetic characters
+# Step 6 - Rename the column names to capitalise Mean and Std, and remove non-alphabetic characters
 names(mean_std_dataset) <- sub('-mean', 'Mean', names(mean_std_dataset))
 names(mean_std_dataset) <- sub('-std', 'Std', names(mean_std_dataset))
 names(mean_std_dataset) <- gsub('[]()-', '', names(mean_std_dataset))
 
-# Introduce the activity names provided from source into the merged data set
+# Step 7 - Add the activity names into the merged data set to aid readability
 dataset_with_activities <- merge(mean_std_dataset, activity_labels, by='activityId', all.x=TRUE)
 
-# Create the second, independent dataset and aggregate the mean of each variable for each activity
+# Step 8 - Create the second, independent dataset, averaging the aggregated values by subject and activity
 second_tidy_set <- aggregate(. ~subjectId + activityId, dataset_with_activities, FUN=mean, na.rm=TRUE)
 write.table(second_tidy_set, 'independent_tidy_set.txt', row.name=FALSE)
 
